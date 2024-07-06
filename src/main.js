fd12b89d15752e64ad7d3d2b0f1ad481916a62ea
ShahriarCode <shahriarcode69@gmail.com>
2024-07-06 22:26:45 +0600

    okay..

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import '../styles/components/header.css'
 import '../styles/components/hero.css'
 import '../styles/components/popular.css'
 import '../styles/components/about.css'
+import '../styles/components/menu.css'
 
 import '../styles/components/mobile-nav.css'
 import '../styles/utils.css'
