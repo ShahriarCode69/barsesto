4f0aef098c3cc88730c6591d892946a6dccb0fdf
ShahriarCode <shahriarcode69@gmail.com>
2024-07-13 11:33:50 +0600

    a

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
@@ -7,13 +6,11 @@ import '../styles/components/popular.css'
 import '../styles/components/about.css'
 import '../styles/components/menu.css'
 import '../styles/components/reserve.css'
-import '../styles/components/reviews.css'
 import '../styles/components/contact.css'
 import '../styles/components/error200.css'
+
 import '../styles/components/mobile-nav.css'
 import '../styles/utils.css'
 
-import reviews from './utils/reviews.js';
-reviews();
-import mobileNav from './utils/mobile-nav';
+import mobileNav from './utils/mobile-nav.js';
 mobileNav();
