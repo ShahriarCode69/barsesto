b059bed8fc776fbb9e74599f2d36c68c6388c1c2
ShahriarCode <shahriarcode69@gmail.com>
2024-07-30 16:39:14 +0600

    aaa

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import '../styles/components/popular.css'
 import '../styles/components/about.css'
 import '../styles/components/menu.css'
 import '../styles/components/reserve.css'
-import '../styles/components/error200.css'
 
 import '../styles/components/mobile-nav.css'
 import '../styles/utils.css'
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 import mobileNav from './utils/mobile-nav.js';
 mobileNav();
