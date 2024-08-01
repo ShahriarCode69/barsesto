4aef8e2f61d77359949a83c824a9f425db4bc76e
ShahriarCode <shahriarcode69@gmail.com>
2024-08-01 08:49:52 +0600

    new chat box added

--- src/main.js
+++ src/main.js
@@ -15,13 +15,13 @@ import '../styles/utils.css'
 import mobileNav from './utils/mobile-nav.js';
 mobileNav();
 
-ScrollReveal().reveal('.header', { origin: 'right', delay: "100", scale: "1.1" });
-ScrollReveal().reveal('.hero', { origin: 'right', delay: "100", scale: "1.1" });
-ScrollReveal().reveal('.about__content', { origin: 'right', delay: "100", scale: "1.1" });
-ScrollReveal().reveal('.about__img', { origin: 'right', delay: "100", scale: "1.1"});
-ScrollReveal().reveal('.reserve__content', { origin: 'right', delay: "100", scale: "1.1"});
-ScrollReveal().reveal('.reserve__img', { origin: 'right', delay: "100", scale: "1.1", rotate: {
-  y: 20,
-}});
+ScrollReveal().reveal('.header', { origin: 'bottom', delay: "100", scale: "1.1", mobile: false });
+ScrollReveal().reveal('.hero', { origin: 'bottom', delay: "100", scale: "1.1", mobile: false });
+ScrollReveal().reveal('.about__content', { origin: 'bottom', delay: "100", scale: "1.1", mobile: false });
+ScrollReveal().reveal('.about__img', { origin: 'bottom', delay: "100", scale: "1.1", mobile: false });
+ScrollReveal().reveal('.reserve__content', { origin: 'bottom', delay: "100", scale: "1.1", mobile: false});
+ScrollReveal().reveal('.reserve__img', { origin: 'bottom', delay: "100", scale: "1.1", rotate: {
+  y: 20
+}, mobile: false});
 
-ScrollReveal().reveal('.popular__card', {delay: 50, easing: "ease-out", interval: 100, scale: 1.2})
+ScrollReveal().reveal('.popular__card', {delay: 50, easing: "ease-out", interval: 100, scale: 1.2, mobile: false})
